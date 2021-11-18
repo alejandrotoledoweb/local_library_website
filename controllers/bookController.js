dd753ceb5ee8ff030d308596def6c8ec3a0a9fb9
alejandro toledo <atoledofr@gmail.com>
2021-11-17 22:14:50 -0500

    update the book_list function

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -30,10 +30,19 @@ exports.index = function(req, res) {
 };
 
 
-// Display list of all books.
-exports.book_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book list');
-};
+// Display list of all Books.
+exports.book_list = function(req, res, next) {
+
+    Book.find({}, 'title author')
+      .sort({title : 1})
+      .populate('author')
+      .exec(function (err, list_books) {
+        if (err) { return next(err); }
+        //Successful, so render
+        res.render('book_list', { title: 'Book List', book_list: list_books });
+      });
+  
+  };
 
 // Display detail page for a specific book.
 exports.book_detail = function(req, res) {
