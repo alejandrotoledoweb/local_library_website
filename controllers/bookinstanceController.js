4929257063670c4e37157be82386db735a8531c2
alejandro toledo <atoledofr@gmail.com>
2021-11-22 11:34:04 -0500

    update the bookinstance method in controller and add the view for that method

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -11,11 +11,23 @@ exports.bookinstance_list = function(req, res, next) {
         res.render('bookinstance_list', { title: 'Book Instance List', bookinstance_list: list_bookinstances });
       });
   
-  };
+};
 
 // Display detail page for a specific BookInstance.
-exports.bookinstance_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance detail: ' + req.params.id);
+exports.bookinstance_detail = function(req, res, next) {
+
+    BookInstance.findById(req.params.id)
+    .populate('book')
+    .exec(function (err, bookinstance) {
+      if (err) { return next(err); }
+      if (bookinstance==null) { // No results.
+          var err = new Error('Book copy not found');
+          err.status = 404;
+          return next(err);
+        }
+      // Successful, so render.
+      res.render('bookinstance_detail', { title: 'Copy: '+bookinstance.book.title, bookinstance:  bookinstance});
+    })
 };
 
 // Display BookInstance create form on GET.
