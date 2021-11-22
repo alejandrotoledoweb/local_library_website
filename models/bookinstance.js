136a8ee5c5c4c4ffdab85829fbf10c52f4727ced
alejandro toledo <atoledofr@gmail.com>
2021-11-22 10:16:16 -0500

    add the virtual property due_back_formatted

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+const { DateTime } = require("luxon");
 
 var Schema = mongoose.Schema;
 
@@ -16,6 +17,12 @@ BookInstanceSchema
 .virtual('url')
 .get(function () {
   return '/catalog/bookinstance/' + this._id;
+});
+
+BookInstanceSchema
+.virtual('due_back_formatted')
+.get(function () {
+  return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
 });
 
 //Export model
