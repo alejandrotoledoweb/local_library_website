75f7bfffdc6dbf7cf93b94d460ea1ac7b6e91b23
alejandro toledo <atoledofr@gmail.com>
2021-11-23 16:52:57 -0500

    update the genre_create_post method in genre controller

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -46,9 +46,51 @@ exports.genre_create_get = function(req, res) {
 };
 
 // Handle Genre create on POST.
-exports.genre_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre create POST');
-};
+// Handle Genre create on POST.
+exports.genre_create_post =  [
+
+    // Validate and santize the name field.
+    body('name', 'Genre name required').trim().isLength({ min: 1 }).escape(),
+  
+    // Process request after validation and sanitization.
+    (req, res, next) => {
+  
+      // Extract the validation errors from a request.
+      const errors = validationResult(req);
+  
+      // Create a genre object with escaped and trimmed data.
+      var genre = new Genre(
+        { name: req.body.name }
+      );
+  
+      if (!errors.isEmpty()) {
+        // There are errors. Render the form again with sanitized values/error messages.
+        res.render('genre_form', { title: 'Create Genre', genre: genre, errors: errors.array()});
+        return;
+      }
+      else {
+        // Data from form is valid.
+        // Check if Genre with same name already exists.
+        Genre.findOne({ 'name': req.body.name })
+          .exec( function(err, found_genre) {
+             if (err) { return next(err); }
+  
+             if (found_genre) {
+               // Genre exists, redirect to its detail page.
+               res.redirect(found_genre.url);
+             }
+             else {
+  
+               genre.save(function (err) {
+                 if (err) { return next(err); }
+                 // Genre saved. Redirect to genre detail page.
+                 res.redirect(genre.url);
+               });
+             }
+           });
+      }
+    }
+  ];
 
 // Display Genre delete form on GET.
 exports.genre_delete_get = function(req, res) {
