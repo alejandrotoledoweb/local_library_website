f9dc06d5bd8499d941c148bc948c0c3e471c9b65
alejandro toledo <atoledofr@gmail.com>
2021-11-23 16:50:13 -0500

    udpate genre_create_post method in genre controller

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,7 +1,8 @@
 var Genre = require('../models/genre');
 var Book = require('../models/book');
 var async = require('async');
-var mongoose = require('mongoose');
+// var mongoose = require('mongoose');
+const { body, validationResult } = require('express-validator');
 
 // Display list of all Genre.
 exports.genre_list = function(req, res) {
@@ -41,7 +42,7 @@ exports.genre_detail = function(req, res, next) {
 };
 // Display Genre create form on GET.
 exports.genre_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre create GET');
+    res.render('genre_form', {title: 'Create Genre'});
 };
 
 // Handle Genre create on POST.
