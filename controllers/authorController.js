88d7fea7e01f3d58898da2c37cbf4b5043ea4d65
alejandro toledo <atoledofr@gmail.com>
2021-11-24 11:26:47 -0500

    update the delete get and post methods in author controller

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -109,9 +109,33 @@ exports.author_delete_get = function(req, res, next) {
 
 };
 
-//Handle Author delete on POST
-exports.author_delete_post = function(req, res) {
-  res.send('NOT IMPLEMENTED: Author delete POST');
+// Handle Author delete on POST.
+exports.author_delete_post = function(req, res, next) {
+
+  async.parallel({
+      author: function(callback) {
+        Author.findById(req.body.authorid).exec(callback)
+      },
+      authors_books: function(callback) {
+        Book.find({ 'author': req.body.authorid }).exec(callback)
+      },
+  }, function(err, results) {
+      if (err) { return next(err); }
+      // Success
+      if (results.authors_books.length > 0) {
+          // Author has books. Render in same way as for GET route.
+          res.render('author_delete', { title: 'Delete Author', author: results.author, author_books: results.authors_books } );
+          return;
+      }
+      else {
+          // Author has no books. Delete object and redirect to the list of authors.
+          Author.findByIdAndRemove(req.body.authorid, function deleteAuthor(err) {
+              if (err) { return next(err); }
+              // Success - go to author list
+              res.redirect('/catalog/authors')
+          })
+      }
+  });
 };
 
 //Display Author update form in GET
