be28fa7a568e555f88caca379c5f0a2ac84f7446
alejandro toledo <atoledofr@gmail.com>
2021-11-24 12:27:52 -0500

    update the genre detail get method and the views for details

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -17,29 +17,32 @@ exports.genre_list = function(req, res) {
 // Display detail page for a specific Genre.
 exports.genre_detail = function(req, res, next) {
 
-    async.parallel({
-        genre: function(callback) {
-            Genre.findById(req.params.id)
-              .exec(callback);
-        },
+  async.parallel({
+      genre: function(callback) {
 
-        genre_books: function(callback) {
-            Book.find({ 'genre': req.params.id })
-              .exec(callback);
-        },
+          Genre.findById(req.params.id)
+            .exec(callback);
+      },
 
-    }, function(err, results) {
-        if (err) { return next(err); }
-        if (results.genre==null) { // No results.
-            var err = new Error('Genre not found');
-            err.status = 404;
-            return next(err);
-        }
-        // Successful, so render
-        res.render('genre_detail', { title: 'Genre Detail', genre: results.genre, genre_books: results.genre_books } );
-    });
+      genre_books: function(callback) {
+        Book.find({ 'genre': req.params.id })
+        .exec(callback);
+      },
+
+  }, function(err, results) {
+      if (err) { return next(err); }
+      if (results.genre==null) { // No results.
+          var err = new Error('Genre not found');
+          err.status = 404;
+          return next(err);
+      }
+      // Successful, so render.
+      res.render('genre_detail', { title: 'Genre Detail', genre: results.genre, genre_books: results.genre_books } );
+  });
 
 };
+
+
 // Display Genre create form on GET.
 exports.genre_create_get = function(req, res) {
     res.render('genre_form', {title: 'Create Genre'});
--- models/genre.js
+++ models/genre.js
@@ -2,14 +2,16 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
-var GenreSchema = new Schema(
-  {
-    name: {type: String, required: true, minLength: 3, maxLength: 100},
-  }
-)
-
-GenreSchema.virtual('url').get(function(){
-  return 'catalog/genre/' + this._id;
+var GenreSchema = new Schema({
+    name: {type: String, required: true, minLength: 3, maxLength: 100}
 });
 
+// Virtual for this genre instance URL.
+GenreSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/genre/'+this._id;
+});
+
+// Export model.
 module.exports = mongoose.model('Genre', GenreSchema);
